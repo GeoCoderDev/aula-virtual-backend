437fa37bd39837cb84ab149a772b396f75f2d2d2
Juanito3500 <juanchavezsaldana1@gmail.com>
2024-05-03 02:24:47 -0500

    ruta de api/students corregida

--- routes/students/index.php
+++ routes/students/index.php
@@ -6,19 +6,22 @@ require_once __DIR__ . "/../../controllers/Estudiante.php";
 
 Flight::group("/api/students",  function(){
 
-    Flight::route("GET ", function(){
+    Flight::route("GET ", function() {
+
+        header("Access-Control-Allow-Origin: *");
+        header('Access-Control-Allow-Headers: Authorization, Content-Type');
     // Obtener los parámetros de consulta de la URL
-    $startFrom = Flight::request()->query['startFrom'] ?? 0;
-    $limit = Flight::request()->query['limit'] ?? 200;
+        $startFrom = Flight::request()->query['startFrom'] ?? 0;
+        $limit = Flight::request()->query['limit'] ?? 200;
 
-    // Convertir a entero si es una cadena
-    $startFrom = intval($startFrom);
-    $limit = intval($limit);
+        // Convertir a entero si es una cadena
+        $startFrom = intval($startFrom);
+        $limit = intval($limit);
 
-    $controller = new EstudianteController();
-    // Pasar los parámetros a tu método getAll
-    Flight::json($controller->getAll(false,$startFrom, $limit), 200);
-});
+        $controller = new EstudianteController();
+        // Pasar los parámetros a tu método getAll
+        Flight::json($controller->getAll(false, $limit, $startFrom), 200);
+    });
 
 
 
