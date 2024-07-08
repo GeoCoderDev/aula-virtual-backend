95f03b21b8ec81f016ff0bfef7731d7443200ae8
Juanito3500 <juanchavezsaldana1@gmail.com>
2024-07-07 22:09:35 -0500

    Correcion al momento de hacer login para obtener los nombres y apellidos del usuario

--- routes/auth/login/users.php
+++ routes/auth/login/users.php
@@ -1,30 +1,27 @@
 <?php
 
-require_once __DIR__."/../../../controllers/Usuario.php";
-require_once __DIR__."/../../../middlewares/isNotSQLInjection.php";
+require_once __DIR__ . "/../../../controllers/Usuario.php";
+require_once __DIR__ . "/../../../middlewares/isNotSQLInjection.php";
 
-Flight::group("/api/auth/login",function(){
+Flight::group("/api/auth/login", function () {
 
-    Flight::route("POST ", function(){
-        header("Access-Control-Allow-Origin: ".ALLOWED_ORIGINS);  
+    Flight::route("POST ", function () {
+        header("Access-Control-Allow-Origin: " . ALLOWED_ORIGINS);
         $controller = new UsuarioController();
-        $data = Flight::request()->data->getData(); 
-        
+        $data = Flight::request()->data->getData();
+
         // Llama a una función para validar el usuario
         $validateResponse = $controller->validateUser($data);
-    
-        if (is_array($validateResponse) && isset($validateResponse['token'])) {    
+
+        if (is_array($validateResponse) && isset($validateResponse['token'])) {
             // Devuelve el token JWT en la respuesta junto con el rol
-            Flight::json(["message" => "Usuario autenticado", "token" => $validateResponse['token'], "role" => $validateResponse['role'], "urlImage"=>$validateResponse['urlImage']??null], 200);
+            Flight::json(["message" => "Usuario autenticado", "token" => $validateResponse['token'], "role" => $validateResponse['role'], "urlImage" => $validateResponse['urlImage'] ?? null, "Nombres" => $validateResponse["Nombres"], "Apellidos" => $validateResponse["Apellidos"]], 200);
         } else {
             if ($validateResponse === 1) {
                 Flight::json(["message" => "Nombre de usuario y contraseña son obligatorios"], 400);
-            } else if($validateResponse === 2) {
+            } else if ($validateResponse === 2) {
                 Flight::json(['message' => 'Nombre de Usuario y/o Contraseña incorrectos'], 401);
             }
         }
     });
-
-},[new NotSQLInjection()]);
-
-?>
+}, [new NotSQLInjection()]);
